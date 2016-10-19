1f922bd844c82152f5b00c87167473aebb43a430
Mike Potter <mpotter@phase2technology.com>
2016-10-19 15:27:16 +0000

    Add some more comments

--- test_decorator_protect/src/TestDecoratorProtect.php
+++ test_decorator_protect/src/TestDecoratorProtect.php
@@ -25,15 +25,23 @@ class TestDecoratorProtect extends TestDecoratorCore {
    */
   public function publicFunction() {
     print "Decorator PROTECT Public\n";
-    // Note we need to call parent::public here to get our overridden
-    // protected function to be called.  If we used $this->testDecorator
-    // then the protected method in the chained service will be run instead
-    // of ours.
+    // NOTE: We need to call parent::public here to get our overridden
+    // protected function to be called.
     parent::publicFunction();
+    // NOTE: You should NOT do the following:
+    // $this->testDecorator->publicFunction()
+    // because then it will call the protected function of the inner service
+    // rather than calling our override below.
   }
 
   protected function protectedFunction() {
     print "Decorator PROTECT Protected\n";
+    // You can still call the parent protected function if you don't need
+    // to completely replace it.
+    parent::protectedFunction();
+    // NOTE: You CANNOT do the following:
+    // $this->testDecorator->protectedFunction()
+    // Because you can only call public methods of the inner decorator.
   }
 
 }
